f4017c46f131cf1a9da84061b3a812818e46bdf6
HuongNguyen0706 <nguyenthuyhuong234@gmail.com>
2016-06-11 11:58:22 +0700

    [Add Project ColorButtonChange]
    use PropertyAnimation on android

---
+++ ColorButtonChange/app/src/main/java/vn/edu/techkids/colorbuttonchange/MainActivity.java
@@ -0,0 +1,44 @@
+package vn.edu.techkids.colorbuttonchange;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btnChangeColor);
+
+        ValueAnimator groundAnimator = ObjectAnimator.ofInt
+                (btn, "backgroundColor",
+                        Color.GREEN, Color.RED);
+        //set same duration and animation properties as others
+        groundAnimator.setDuration(3000);
+        groundAnimator.setEvaluator(new ArgbEvaluator());
+        groundAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        groundAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+        groundAnimator.start();
+
+
+    }
+
+}
