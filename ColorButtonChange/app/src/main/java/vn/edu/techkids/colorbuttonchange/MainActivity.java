dca05d9c2aa0194aa10794405fc26767f794d4a5
HuongNguyen0706 <nguyenthuyhuong234@gmail.com>
2016-06-11 22:42:26 +0700

    [update]

--- ColorButtonChange/app/src/main/java/vn/edu/techkids/colorbuttonchange/MainActivity.java
+++ ColorButtonChange/app/src/main/java/vn/edu/techkids/colorbuttonchange/MainActivity.java
@@ -13,12 +13,15 @@ import android.text.SpannableString;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    Button btn, test;
 
 
     @Override
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button)findViewById(R.id.btnChangeColor);
+        test = (Button)findViewById(R.id.btnTest);
 
         final ValueAnimator groundAnimator = ObjectAnimator.ofInt
                 (btn, "backgroundColor",
@@ -40,6 +44,33 @@ public class MainActivity extends AppCompatActivity {
         groundAnimator.start();
 
 
+        doTranslate();
+
+
+
+    }
+
+    public void doTranslate(){
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) test.getLayoutParams();
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        test.setLayoutParams(layoutParams);
+        test.clearAnimation();
+
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.layout);
+        int top = relativeLayout.getTop();
+        int left = relativeLayout.getLeft();
+        int bottom = relativeLayout.getBottom();
+        int right = relativeLayout.getRight();
+
+        //do the animation stuff
+        TranslateAnimation translateAnimation = new TranslateAnimation(left, right, top, bottom);
+        translateAnimation.setDuration(4000);
+        translateAnimation.setRepeatCount(1);
+        translateAnimation.setInterpolator(new AccelerateInterpolator());
+
+
+        test.startAnimation(translateAnimation);
     }
 
 }
